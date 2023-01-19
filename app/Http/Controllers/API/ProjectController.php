e00ca74c712ee37a51af86d5c7fafdd39e063e4f
sensiz <dev.federicosensi@gmail.com>
2023-01-19 15:51:43 +0100

    Milestones 1, 2 complete

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+            'results' => Project::with(['type', 'technologies'])->orderByDesc('id')->paginate(5)
+        ]);
+    }
+}
